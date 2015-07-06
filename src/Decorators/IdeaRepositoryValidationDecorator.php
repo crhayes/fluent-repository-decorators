5e94a4714ba317970fa3de17ae640c1352f81622
Chris Hayes <chris@moderncognition.com>
2015-07-06 14:01:56 -0400

    Add example validation decorator

---
+++ src/Decorators/IdeaRepositoryValidationDecorator.php
@@ -0,0 +1,51 @@
+<?php namespace App\Decorators;
+
+use App\Contracts\RepositoryInterface;
+
+class IdeaRepositoryValidationDecorator implements RepositoryInterface {
+
+	protected $repository;
+	protected $validator;
+
+	public function __construct(RepositoryInterface $repository, IdeaValidator $validator) {
+		$this->repository = $repository;
+		$this->validator = $validator;
+	}
+
+	public function get($columns) {
+		return $this->repository->get($columns);
+	}
+
+	public function find($id, $columns) {
+		return $this->repository->find($id, $columns);
+	}
+
+	public function paginate($perPage, $page, $columns) {
+		return $this->repository->paginate($perPage, $page, $columns);
+	}
+
+	public function chunk($perChunk, Closure $callback) {
+		return $this->repository->chunk($perChunk, $callback);
+	}
+
+	public function save(Model $model) {
+		if ( ! isset($model->id) && ! $this->validator->validateSave($model) ) {
+			throw new ValidationException($this->validator->getErrors());
+		}
+
+		if (isset($model->id) && ! $this->validator->validateUpdate($model)) {
+			throw new ValidationException($this->validator->getErrors());
+		}
+
+		return $this->repository->save($model);
+	}
+
+	public function delete(Model $model) {
+		return $this->repository->delete($model);
+	}
+
+	public function purge(Model $model) {
+		return $this->repository->purge($model);
+	}
+
+}
